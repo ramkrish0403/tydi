d6ae1da63be4cca5deeb2c50c46618e7f862df62
ramkrish0403 <ramkrish0403@gmail.com>
2024-09-19 12:02:34 +0000

    misc

--- src/tydi/__init__.py
+++ src/tydi/__init__.py
@@ -1,4 +1,4 @@
 from .dispatch import dispatch
-from .overload import get_overloads, overload
+# from .overload import get_overloads, overload
 
-__all__ = ["overload", "get_overloads", "dispatch"]
+__all__ = ["dispatch"]
